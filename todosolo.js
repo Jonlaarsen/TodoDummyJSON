d8b53801ef956104acebebee264cb67f37033ce0
Jon Larsen <jonlarsen@Jons-MacBook-Air.local>
2023-06-20 13:07:33 +0900

    main

--- todosolo.js
+++ todosolo.js
@@ -4,9 +4,45 @@ let addInput = document.querySelector("#add-input");
 let addHeader = document.querySelector("#add-header");
 
 
+// show all todos
+fetch("https://dummyjson.com/todos")
+  .then((res) => res.json())
+  .then((data) => {
+    let mytodos = data.todos;
+    let item = "";
+    for (let i = 0; i < mytodos.length; i++) {
+      
+      let date = new Date();   
+        newDate = date.toLocaleString();   
+        
+        
+        item += `
+        <li class="todo-li" ${data.completed = false} id="${mytodos[i].id}">
+                  <p class="header">RANDOM</p>
+                  <p class="content">${mytodos[i].todo}</p>
+                  <div class="buttons">
+                    <p class="createdDate">${newDate}</p>
+                    <button class="finished">Finished</button>
+                    <button class="delete">Delete</button>
+                    
+                  </div>
+                </li> 
+                `;
+    }
+    return item;
+  })
+  .then((item) => {
+    list.innerHTML += item;
+  })
+  .then(() => {
+    deleteTodos();
+    finishedEvent();
+  })
+  .catch((err) => {
+    console.error("Error", err);
+  });
 
-  let date = new Date();   
-  newDate = date.toLocaleString();      
+   
 
  
 //a warning sign to put in  both a task and title.
@@ -16,7 +52,7 @@ addBtn.addEventListener("click", () => {
         alert("add title or task!"); 
         return;
       };
-      
+   
     addTodo(addInput.value);
   });
 
@@ -34,17 +70,20 @@ function addTodo() {
     })
       .then((res) => res.json())
       .then((data) => {
+
+        let date = new Date();   
+        newDate = date.toLocaleString();   
         
         let item = "";
         item += `
-        <li class="todo-li" ${data.completed = 'false'} id="${data.id=1}">
+        <li class="todo-li" ${data.completed = false} id="${data.id=31}">
                   <p class="header">${addHeader.value}</p>
                   <p class="content">${data.todo}</p>
                   <div class="buttons">
                     <p class="createdDate">${newDate}</p>
                     <button class="finished">Finished</button>
                     <button class="delete">Delete</button>
-                    <p class="finishedDate">null</p>
+                    
                   </div>
                 </li> 
                 `;
@@ -62,17 +101,18 @@ function addTodo() {
               
           };
 
- // function that changes tasks class and completed status.         
+ // function that changes tasks class, completed status to current time.         
 
 function finishedEvent() {
   let finishedplural = document.querySelectorAll(".finished");
-  let finishedDate = document.querySelectorAll(".finishedDate")
+  
 
   finishedplural.forEach((finished) => {
     finished.addEventListener("click", () => {
       if (finished.classList[0] === "finishedPressed") {
         finished.classList.remove("finishedPressed");
         finished.classList.add("finished");
+        finished.innerHTML =  "FINISHED";
         fetch(`https://dummyjson.com/todos/${finished.parentElement.parentElement.id}`, {
           method: "PUT" /* or PATCH */,
           headers: { "Content-Type": "application/json" },
@@ -87,16 +127,9 @@ function finishedEvent() {
       } else if (finished.classList[0] === "finished") {
         finished.classList.add("finishedPressed");
         finished.classList.remove("finished");
-       
-       /*
-        let finishedDate = document.querySelectorAll(".createdDate");
-        finishedDate.forEach((finishedD) => {
-        let date = new Date();
-        newDate = date.toLocaleString();
-        finishedD.innerHTML = newDate; 
-        });*/
-
-        console.log(newDate)
+        let finishedDate =  new Date();
+        let newFinishedDate = finishedDate.toLocaleString();
+        finished.innerHTML = newFinishedDate; 
 
         fetch(`https://dummyjson.com/todos/${finished.parentElement.parentElement.id}`, {
           method: "PUT" /* or PATCH */,
@@ -114,7 +147,9 @@ function finishedEvent() {
       }
       
     });
+    
   });
+  
 } 
 
 //function that deletes todos.
